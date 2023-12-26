3e915085d71d77dfb31d20758b6bedc80c3b9a06
UNCOMMONS <118422255+uncms@users.noreply.github.com>
2023-12-26 23:57:32 +0900

    Add files via upload

---
+++ js/mint.js
@@ -0,0 +1,15 @@
+function home(){window.scrollTo(0,0);}
+
+function alertSample(_num){
+    switch(_num){
+        case 0:
+            alert("메타마스크를 연동하여, 지갑주소와 보유잔액을 호출합니다.")
+            break;
+        case 1:
+            alert("사전에 설정된 판매가격으로 NFT를 구매합니다.")
+            break;
+        case 2:
+            alert("세계 최대 NFT 거래소인 오픈씨에서 구매한 NFT를 확인합니다.")
+            break;
+    }
+}
