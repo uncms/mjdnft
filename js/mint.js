41d2146fe2c3eb9b1747fac88659158d8b337c1d
UNCOMMONS <118422255+uncms@users.noreply.github.com>
2024-01-03 14:50:34 +0900

    Add files via upload

--- js/mint.js
+++ js/mint.js
@@ -1,15 +1,103 @@
 function home(){window.scrollTo(0,0);}
 
-function alertSample(_num){
-    switch(_num){
-        case 0:
-            alert("메타마스크를 연동하여, 지갑주소와 보유잔액을 호출합니다.")
-            break;
-        case 1:
-            alert("사전에 설정된 판매가격으로 NFT를 구매합니다.")
-            break;
-        case 2:
-            alert("세계 최대 NFT 거래소인 오픈씨에서 구매한 NFT를 확인합니다.")
-            break;
+let web3;
+let account;
+let balance;
+let mintPrice;
+
+const walletObj = document.querySelector(".wallet");
+const balanceObj = document.querySelector(".balance");
+const priceObj = document.querySelector(".price");
+const gasfeeObj = document.querySelector(".gasfee");
+const openseaObj = document.querySelector(".opensea");
+
+window.onload = async function () {
+    web3 = new Web3(window.ethereum);
+    await check_status();
+};
+
+async function connect() {
+    if (window.ethereum) {
+        let isContinue = false;
+
+        await window.ethereum.request({ method: "net_version" }).then((res) => {
+            if( (res != 137) && (res != 80001)){
+                alert("메타마스크 네트워크를 폴리곤으로 변경해주세요");
+            }else{
+                isContinue = true;
+            }
+        });
+
+        if(!isContinue){return;}
+
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts", });
+
+        account = accounts[0];
+
+        balance = await window.ethereum
+            .request({ method: "eth_getBalance", params: [account, "latest"] })
+            .catch((err) => {
+            });
+
+        balance = web3.utils.fromWei(String(balance), "ether");
+        
+        
+        walletObj.innerHTML = "- 지갑주소 : " + account.substr(0, 12) + "...";
+        balanceObj.innerHTML = "- 보유잔액 : " + BigNumber(balance).toFixed(4) + " MATIC";
+
+        await check_status();
+    } else {
+        alert("메타마스크를 설치하고 활성화해주세요");
+        return;
+    }
+}
+
+
+async function check_status() {
+    const contract = new web3.eth.Contract(ABI, CONTRACTADDRESS);
+
+    await contract.methods
+        .mintingInformation()
+        .call()
+        .then(function (res) {
+            mintPrice = parseInt(res[4]);
+
+        priceObj.innerHTML = "- 구매금액 : " + web3.utils.fromWei(String(mintPrice), "ether") + " MATIC";
+        gasfeeObj.innerHTML = "- 예상수수료 : 0.001 ~ 0.01 MATIC"
+    
+        })
+        .catch(function (err) {
+        });
+
+    openseaObj.innerHTML = "- 오픈씨URL : testnets.opensea.io/kr/collection/tmjd"; 
+}
+
+function goOpensea() {
+    window.open("https://testnets.opensea.io/kr/collection/tmjd");
+}
+
+async function publicMint() {
+    if (window.ethereum) {
+        await connect();
+    } else {
+        alert("메타마스크를 설치하고 활성화해주세요");
+        return;
+    }
+
+    const contract = new web3.eth.Contract(ABI, CONTRACTADDRESS);
+
+    try {
+        const result = await contract.methods.publicMint(1).send({
+            from: account,
+            value: mintPrice.toString(),
+        });
+
+        if (result != null) {
+            alert("축하합니다! 성공적으로 명주동NFT를 구매하였습니다");
+            await connect();
+        }
+    } catch (err) {
+        alert("민팅에 실패하였습니다. 잠시 뒤 재시도 해주시기 바랍니다");
+        await connect();
     }
 }
